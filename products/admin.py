485c91a4d882d39e2a8eb17c54ad79bc2e19b8ab
kuzGo <gorankuzma467@gmail.com>
2021-11-03 23:59:33 +0000

    Add product fixtures and customize admin

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
 from .models import Product, Category
 
-admin.site.register(Product)
-admin.site.register(Category)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'price',
+        'rating',
+        'image',
+        'gender',
+    )
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'ui_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
