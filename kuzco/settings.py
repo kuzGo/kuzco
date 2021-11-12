9cbea331b5d7d833b5c8c24abdf5d04d019bbb7f
kuzGo <gorankuzma467@gmail.com>
2021-11-12 02:07:59 +0000

    Add context to shopping cart

--- kuzco/settings.py
+++ kuzco/settings.py
@@ -98,6 +98,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context.cart_items',
             ],
         },
     },
@@ -157,6 +158,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_THRESHOLD = 100
+STANDARD_DELIVERY_PERCENTAGE = 10
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
