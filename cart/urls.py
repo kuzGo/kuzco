be19c15c761da66c58d37fd7a13cad39051dff3f
kuzGo <gorankuzma467@gmail.com>
2021-11-15 23:47:00 +0000

    Implement adding items to the shopping cart functionality

--- cart/urls.py
+++ cart/urls.py
@@ -6,4 +6,5 @@ from . import views
 
 urlpatterns = [
     path('', views.shopping_cart, name='shopping_cart'),
+    path('add/<item_id>', views.add_to_cart, name='add_to_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,9 +1,25 @@
 """
 This module returns the shopping cart page view
 """
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def shopping_cart(request):
     """ Returns shopping cart view"""
     return render(request, 'cart/shopping_cart.html')
+
+
+def add_to_cart(request, item_id):
+    """Adds a quantity of the items to the shopping cart"""
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_url)
