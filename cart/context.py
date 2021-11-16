c0f311c324f6f70faf407fed721cb80cb548779d
kuzGo <gorankuzma467@gmail.com>
2021-11-16 01:21:48 +0000

    Add table to the shopping_cart.html to display details of the items

--- cart/context.py
+++ cart/context.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def cart_items(request):
@@ -7,6 +9,17 @@ def cart_items(request):
     cart_contents = []
     total = 0
     product_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        watch = get_object_or_404(Product, pk=item_id)
+        total += quantity * watch.price
+        product_count += quantity
+        cart_contents.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'watch': watch,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
