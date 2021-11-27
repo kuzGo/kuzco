bbdee8127df17b670004bcd1adcf161d65f066a9
kuzGo <gorankuzma467@gmail.com>
2021-11-27 23:30:19 +0000

    Add Stipe javascript and css to checkout app, installed Stripe

---
+++ checkout/static/checkout/javascript/stripe.js
@@ -0,0 +1,27 @@
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
+var elements = stripe.elements();
+var card = elements.create('card', { style: style });
+
+var style = {
+    base: {
+      iconColor: '#c4f0ff',
+      color: '#fff',
+      fontWeight: '500',
+      fontFamily: 'Roboto, Open Sans, Segoe UI, sans-serif',
+      fontSize: '16px',
+      fontSmoothing: 'antialiased',
+      ':-webkit-autofill': {
+        color: '#fce883',
+      },
+      '::placeholder': {
+        color: '#87BBFD',
+      },
+    },
+    invalid: {
+      iconColor: '#FFC7EE',
+      color: '#FFC7EE',
+    },
+  };
+  card.mount('#card-element');
--- checkout/views.py
+++ checkout/views.py
@@ -1,19 +1,27 @@
 from django.shortcuts import render, reverse, redirect
-
 from django.contrib import messages
 from django.conf import settings
 from .forms import OrderForm
+from cart.context import cart_items
+
+import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "Your cart is currently empty.")
         return redirect(reverse('shopping_cart'))
 
+    existsing_cart = cart_items(request)
+    total = existsing_cart['grand_total']
     order_form = OrderForm()
+    stripe_total = round(total * 100)
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': settings.STRIPE_PUBLIC_KEY
     }
     return render(request, template, context)
