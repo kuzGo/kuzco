841e4b69701e751de7a15b74e2ba800a8a037825
kuzGo <gorankuzma467@gmail.com>
2021-12-01 00:39:40 +0000

    Implement basic Stripe webhook event listener

--- checkout/urls.py
+++ checkout/urls.py
@@ -3,9 +3,11 @@ Provides URL path to the checkout page
 """
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('successful_checkout/<order_number>',
          views.successful_checkout, name='successful_checkout'),
+    path('wh/', webhook, name='webhook'),     
 ]
