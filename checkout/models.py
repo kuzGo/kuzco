ceec52d1bb75440f3f2e4d28ac580ee3dc483d0e
kuzGo <gorankuzma467@gmail.com>
2021-12-07 23:36:44 +0000

    Complete handling of  the webhooks

--- checkout/models.py
+++ checkout/models.py
@@ -28,6 +28,8 @@ class Order(models.Model):
         max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(
         max_digits=10, decimal_places=2, null=False, default=0)
+    original_cart = models.TextField(null=False, blank=False, default='')
+    stripe_pay_id = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def _order_number_generator(self, *args, **kwargs):
         """ Generates unique order number using UUID"""
--- checkout/views.py
+++ checkout/views.py
@@ -50,9 +50,13 @@ def checkout(request):
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            payment_intent_id = request.POST.get(
+                'client_secret').split('_secret')[0]
+            order.stripe_pay_id = payment_intent_id
+            order.original_cart = json.dumps(cart)
+            order.save()
             for item_id, item_data in cart.items():
-
                 try:
                     product = Product.objects.get(id=item_id)
                     if isinstance(item_data, int):
