1abb24de72225d2895aa6b073b0ca46749735bd4
kuzGo <gorankuzma467@gmail.com>
2021-12-11 23:06:35 +0000

    Implement sending emails functionality

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,15 +1,35 @@
 import json
 import time
-
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
 from django.http import HttpResponse
 from products.models import Product
 from .models import Order, OrderLineItem
 from profiles.models import UserProfile
 
+from django.conf import settings
+
 
 class StripeWH_Handler:
     def __init__(self, request):
         self.request = request
+
+    def _send_confirmation_email(self, order):
+        """ Sends a confirmation email """
+        customer_email = order.email
+        subject = render_to_string(
+            'checkout/emails/email_subject.txt',
+            {'order': order})
+        body = render_to_string(
+            'checkout/emails/email_body.txt',
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [customer_email]
+        )
 
     def handle_event(self, event):
         return HttpResponse(
@@ -68,6 +88,7 @@ class StripeWH_Handler:
                 attempt += 1
                 time.sleep(1)
         if order_exists:
+            self._send_confirmation_email(order)
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | SUCCESS:\
                             Verified order already in database',
@@ -105,6 +126,7 @@ class StripeWH_Handler:
                     content=f'Webhook received: {event["type"]} | ERROR: {er}',
                     status=500)
 
+        self._send_confirmation_email(order)
         return HttpResponse(
             content=(f'Webhook received: {event["type"]} | SUCCESS: '
                      'Created order in webhook'),
