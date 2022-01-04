8d4c82377ac4865cbfeebbaa6bc3d18802664b0b
kuzGo <gorankuzma467@gmail.com>
2022-01-04 13:07:17 +0000

    Implement functionality to remove favourite items

--- favourites/urls.py
+++ favourites/urls.py
@@ -7,4 +7,6 @@ from . import views
 urlpatterns = [
     path('', views.all_favourites, name='all_favourites'),
     path('add/<int:product_id>/', views.add_favourites, name='add_favourites'),
+    path('delete/<int:product_id>/',
+         views.delete_favourites, name='delete_favourites'),
 ]
--- favourites/views.py
+++ favourites/views.py
@@ -42,3 +42,17 @@ def add_favourites(request, product_id):
     favourite.watches.add(watch)
     messages.info(request, "A new item was added to your favourites")
     return redirect(reverse('all_favourites'))
+
+
+@login_required
+def delete_favourites(request, product_id):
+    """
+    A view to remove an item from a list of
+    favourite items
+    """
+    favorite_item = Favourites.objects.get(user=request.user)
+    watch_card = get_object_or_404(Product, pk=product_id)
+    favorite_item.watches.remove(watch_card)
+    messages.info(
+        request, f'{watch_card.name} was removed from your favourites')
+    return redirect(reverse('all_favourites'))
